eaffb73e5a08bcd12d938c508720dbb91b4888cb
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-06-08 10:33:29 +0100

    add Mongoose schema - coach

--- dist/app/models/clientModel.js
+++ dist/app/models/clientModel.js
@@ -1,5 +1,24 @@
 "use strict";
 exports.__esModule = true;
 var mongoose = require("mongoose");
-var clientSchema = new mongoose.Schema({ name: String }, { collection: 'sport' });
+var coach = {
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: Int16Array,
+    dob: Date,
+    houseNumber: Int16Array,
+    houseName: String,
+    address1: String,
+    address2: String,
+    town: String,
+    county: String,
+    postcode: String,
+    qualifications: Array,
+    yearsCoaching: Int16Array,
+    sport: String,
+    expertise: String,
+    password: String //obvs needs sorting out
+};
+var clientSchema = new mongoose.Schema(coach, { collection: 'coach' });
 exports["default"] = mongoose.model('Client', clientSchema);
