79342833ab6c8cff100e6b89f97a118908792e11
Richard Herbert <richard.herbert1980@gmail.com>
2020-06-08 11:39:52 +0100

    change db connection to use split archtiecture

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,10 +1,9 @@
 import express = require('express')
 import routes from './routes'
 import mongoose = require('mongoose')
+import {} from './db'
 const app : express.Application = express()
 const port : number = 3000
-
-mongoose.connect('mongodb://localhost:27017/CoachRed', {useNewUrlParser: true});
 
 app.use(express.json())
 app.use(express.urlencoded())
